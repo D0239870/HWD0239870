88acdf0ad51344d4df693c3646bd9ca62f20ddb5
andy <andy_840123@yahoo.com.tw>
2015-12-21 09:58:33 +0800

    third version add show

--- src/Question.java
+++ src/Question.java
@@ -6,12 +6,16 @@
 abstract class test{
 	abstract void show();
 }
-class Question {
+class Question extends test{
    String description;
    public Question(String desc) {
       this.description = desc;
    }
    public void print() {
       System.out.println(desc);
-   }      
+   }
+void show() {
+	// TODO Auto-generated method stub
+	
+}      
 }
