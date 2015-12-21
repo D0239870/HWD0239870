3e84849219952e452a95395e0737351fcac60ab3
andy <andy_840123@yahoo.com.tw>
2015-12-21 09:56:53 +0800

    second version add abstract class test

--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,17 @@
-
-public class Question {
-
+/*public class demo{
+	public static void main(String[] args){
+		
+	}
+}*/
+abstract class test{
+	abstract void show();
+}
+class Question {
+   String description;
+   public Question(String desc) {
+      this.description = desc;
+   }
+   public void print() {
+      System.out.println(desc);
+   }      
 }
